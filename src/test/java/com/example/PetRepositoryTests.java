a189f28ea287aa993009c2fca81c0a1193440c03
Eric Deandrea <eric.deandrea@gmail.com>
2025-07-09 09:40:43 +0200

    Update PetRepositoryTests

--- src/test/java/com/example/PetRepositoryTests.java
+++ src/test/java/com/example/PetRepositoryTests.java
@@ -15,86 +15,79 @@ class PetRepositoryTests {
 	@Inject
 	PetRepository petRepository;
 
-	@Test
-	void petsByKindFound() {
-		// tag::petsByKindFound[]
+	private void setupTest() {
 		// Delete everything from the repo
 		this.petRepository.deleteAll();
 
 		// Assert the repository is empty
-		assertThat(this.petRepository.count()).isZero();
+		assertThat(this.petRepository.count())
+				.isZero();
+	}
+
+	@Test
+	void petsByKindFound() {
+		// tag::petsByKindFound[]
+		setupTest();
 
 		// Persist a new pet to the repo
 		this.petRepository.persist(new Pet("fluffy", "cat"));
 
 		// Assert that finding the pets by kind returns the correct result
 		assertThat(this.petRepository.findPetsByKind("cat"))
-			.isNotNull()
-			.singleElement()
-			.extracting(Pet::getName, Pet::getKind)
-			.containsExactly("fluffy", "cat");
+				.isNotNull()
+				.singleElement()
+				.extracting(Pet::getName, Pet::getKind)
+				.containsExactly("fluffy", "cat");
 		// end::petsByKindFound[]
 	}
 
 	@Test
 	void noPetsByKindFound() {
 		// tag::noPetsByKindFound[]
-		// Delete everything from the repo
-		this.petRepository.deleteAll();
-
-		// Assert the repository is empty
-		assertThat(this.petRepository.count()).isZero();
+		setupTest();
 
 		// Assert that finding the pets by kind returns empty
 		assertThat(this.petRepository.findPetsByKind("cat"))
-			.isNotNull()
-			.isEmpty();
+				.isNotNull()
+				.isEmpty();
 		// end::noPetsByKindFound[]
 	}
 
 	@Test
 	void adoptFoundPet() {
 		// tag::adoptFoundPet[]
-		// Delete everything from the repo
-		this.petRepository.deleteAll();
-
-		// Assert the repository is empty
-		assertThat(this.petRepository.count()).isZero();
+		setupTest();
 
 		// Persist some pets
 		this.petRepository.persist(
-			new Pet("fluffy", "cat"),
-			new Pet("harry", "dog")
+				new Pet("fluffy", "cat"),
+				new Pet("harry", "dog")
 		);
 
 		// Assert that adopting a found pet is correct
 		assertThat(this.petRepository.adoptPetIfFound("cat", "Eric"))
-			.isNotNull()
-			.get()
-			.extracting(Pet::getKind, Pet::getName, Pet::getAdoptedBy)
-			.containsExactly("cat", "fluffy", "Eric");
+				.isNotNull()
+				.get()
+				.extracting(Pet::getKind, Pet::getName, Pet::getAdoptedBy)
+				.containsExactly("cat", "fluffy", "Eric");
 		// end::adoptFoundPet[]
 	}
 
 	@Test
 	void noAdoptablePetFound() {
 		// tag::noAdoptablePetFound[]
-		// Delete everything from the repo
-		this.petRepository.deleteAll();
-
-		// Assert the repository is empty
-		assertThat(this.petRepository.count()).isZero();
+		setupTest();
 
 		// Persist some pets
 		this.petRepository.persist(
-			new Pet(null, "fluffy", "cat", "Eric"),
-			new Pet("harry", "dog")
+				new Pet(null, "fluffy", "cat", "Eric"),
+				new Pet("harry", "dog")
 		);
 
 		// Assert that no pet is found for adoption
 		assertThat(this.petRepository.adoptPetIfFound("cat", "Eric"))
-			.isNotNull()
-			.isEmpty();
+				.isNotNull()
+				.isEmpty();
 		// end::noAdoptablePetFound[]
 	}
 }
