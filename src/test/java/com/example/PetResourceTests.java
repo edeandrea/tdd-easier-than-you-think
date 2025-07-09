2e53878a558fe5ebcf6a9c21b96087ee7525061d
Eric Deandrea <eric.deandrea@gmail.com>
2025-07-09 09:59:28 +0200

    Update PetResourceTests

--- src/test/java/com/example/PetResourceTests.java
+++ src/test/java/com/example/PetResourceTests.java
@@ -1,13 +1,18 @@
 package com.example;
 
 import static io.restassured.RestAssured.get;
-import static jakarta.ws.rs.core.Response.Status.*;
+import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
+import static jakarta.ws.rs.core.Response.Status.OK;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.blankOrNullString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Optional;
+
+import jakarta.ws.rs.core.Response.Status;
 
 import org.junit.jupiter.api.Test;
 
@@ -26,16 +31,16 @@ class PetResourceTests {
 		// tag::getAll[]
 		// Set up mock to return a pet when repo.listAll() is called
 		when(this.petRepository.listAll())
-			.thenReturn(List.of(new Pet(1L, "fluffy", "cat")));
+				.thenReturn(List.of(new Pet(1L, "fluffy", "cat")));
 
 		// Execute GET to /pets & assert
 		get("/pets").then()
-			.contentType(ContentType.JSON)
-			.statusCode(OK.getStatusCode())
-			.body("$.size()", is(1))
-			.body("[0].name", is("fluffy"))
-			.body("[0].kind", is("cat"))
-			.body("[0].adoptedBy", blankOrNullString());
+				.statusCode(Status.OK.getStatusCode())
+				.contentType(ContentType.JSON)
+				.body("$.size()", is(1))
+				.body("[0].name", is("fluffy"))
+				.body("[0].kind", is("cat"))
+				.body("[0].adoptedBy", blankOrNullString());
 
 		// Verify interactions
 		verify(this.petRepository).listAll();
@@ -47,16 +52,16 @@ class PetResourceTests {
 	void getByKind() {
 		// Set up mock to return a pet when repo.findPetsByKind("cat") is called
 		when(petRepository.findPetsByKind("cat"))
-			.thenReturn(List.of(new Pet(1L, "fluffy", "cat", "Eric")));
+				.thenReturn(List.of(new Pet(1L, "fluffy", "cat", "Eric")));
 
 		// Execute GET to /pets?kind=cat & assert
 		get("/pets?kind={kind}", "cat").then()
-			.contentType(ContentType.JSON)
-			.statusCode(OK.getStatusCode())
-			.body("$.size()", is(1))
-			.body("[0].name", is("fluffy"))
-			.body("[0].kind", is("cat"))
-			.body("[0].adoptedBy", is("Eric"));
+				.contentType(ContentType.JSON)
+				.statusCode(OK.getStatusCode())
+				.body("$.size()", is(1))
+				.body("[0].name", is("fluffy"))
+				.body("[0].kind", is("cat"))
+				.body("[0].adoptedBy", is("Eric"));
 
 		// Verify interactions
 		verify(this.petRepository).findPetsByKind("cat");
@@ -67,15 +72,15 @@ class PetResourceTests {
 	void getByIdFound() {
 		// Set up mock to return a pet when repo.findByIdOptional(1) is called
 		when(petRepository.findByIdOptional(1L))
-			.thenReturn(Optional.of(new Pet(1L, "fluffy", "cat", "Eric")));
+				.thenReturn(Optional.of(new Pet(1L, "fluffy", "cat", "Eric")));
 
 		// Execute GET to /pets/1 & assert OK
 		get("/pets/{id}", 1L).then()
-			.contentType(ContentType.JSON)
-			.statusCode(OK.getStatusCode())
-			.body("name", is("fluffy"))
-			.body("kind", is("cat"))
-			.body("adoptedBy", is("Eric"));
+				.contentType(ContentType.JSON)
+				.statusCode(OK.getStatusCode())
+				.body("name", is("fluffy"))
+				.body("kind", is("cat"))
+				.body("adoptedBy", is("Eric"));
 
 		// Verify interactions
 		verify(this.petRepository).findByIdOptional(1L);
@@ -86,12 +91,12 @@ class PetResourceTests {
 	void getByIdNotFound() {
 		// Set up mock to return empty when repo.findByIdOptional(1) is called
 		when(petRepository.findByIdOptional(1L))
-			.thenReturn(Optional.empty());
+				.thenReturn(Optional.empty());
 
 		// Execute GET to /pets/1 & assert NOT_FOUND
 		get("/pets/{id}", 1L).then()
-			.statusCode(NOT_FOUND.getStatusCode())
-			.body(blankOrNullString());
+				.statusCode(NOT_FOUND.getStatusCode())
+				.body(blankOrNullString());
 
 		// Verify interactions
 		verify(this.petRepository).findByIdOptional(1L);
